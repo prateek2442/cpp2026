07886a5cfb055d4e0281ad610487b6ef63be02a4
Prateek singh <devsin2442@gmail.com>
2026-05-30 19:09:00 +0530

    continuing

---
+++ Project1/Log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void Log(const char* message) {
+	// Log the message to the console
+	std::cout << message << std::endl;
+}
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include"log.h"
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
 
 int main()
-{
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+{	
+
+	std::cout<< "Hello World!" << std::endl;
+
+	int result = multiply(20, 7);
+
+	std::cout << result << std::endl;
+
+	//using header file to print message  from a function defined in another file.
+
+	Log("Hello World from Log");
 }
+
+// printing the basic hello world message and then making a function to understand the syntax and use of function.
